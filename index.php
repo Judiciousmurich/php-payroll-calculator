b26f824dc6a19d3e368b8b473860768e4fb26952
judiciousmurich <murichjudicious45@gmail.com>
2024-04-06 17:51:27 +0300

    feat: add data

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+    $EmployeeName = $_POST['employeeName'];
+    $hoursWorked = $_POST['hoursWorked'];
+    $ratePerHour = $_POST['ratePerHour'];
+
 }
